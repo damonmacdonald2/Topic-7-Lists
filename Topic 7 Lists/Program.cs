156ad568b79955d564936a75da17b51be42cbc6a
Damon MacDonald <339493611@GoTVDSB.ca>
2024-04-17 14:04:06 -0400

    Add project files.

---
+++ Topic 7 Lists/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Topic_7_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> list = new List<int>();
+            Console.WriteLine(":D happy coding dmac");
+            Console.Beep();
+            Console.ReadLine();
+
+        }
+    }
+}
